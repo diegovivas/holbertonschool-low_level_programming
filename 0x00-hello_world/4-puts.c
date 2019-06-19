34d095e7b04663947b9444513978d14c87394ab4
Diego Vivas <dieguitovivas@hotmail.com>
2019-06-19 18:41:30 +0000

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,6 @@
 */
 int main(void)
 {
-char string[40];
-strcpy(str, "Programming is like building a multilingual puzzle");
-puts(string);
+puts("Programming is like building a multilingual puzzle");
 return (0);
 }
