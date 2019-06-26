2a2ab4101643e3ec2fa1ec083ef0ab4cd7e3ce81
diegovivas <dieguitovivas@hotmail.com>
2019-06-26 04:22:46 +0000

    100done fixed

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,45 +6,39 @@
  */
 void print_times_table(int i)
 {
-  if(i < 16 && i > -1)
-    {
-      int a, b, c, d, e, f;
-
-      for (a = 0; a <= i; a++)
+	if (i < 16 && i > -1)
 	{
-	  _putchar('0');
+		int a, b, c, d, e, f;
 
-	  for (b = 1; b <= i; b++)
-	    {
-
-	      c = a * b;
-	      d = c / 100, e = c / 10 % 10, f = c % 10;
-	      _putchar(',');
-	      _putchar(' ');
-
-	      if (c > 99)
+		for (a = 0; a <= i; a++)
+	{
+		_putchar('0');
+		for (b = 1; b <= i; b++)
 		{
-		  _putchar(d + '0');
-		  _putchar(e + '0');
-		  _putchar(f + '0');
-		}
-	      else if (c < 10)
-		{
-		  _putchar(' ');
-		  _putchar(' ');
-		  _putchar(f + '0');
+			c = a * b, d = c / 100, e = c / 10 % 10, f = c % 10;
+			_putchar(',');
+			_putchar(' ');
+			if (c > 99)
+			{
+				_putchar(d + '0');
+				_putchar(e + '0');
+				_putchar(f + '0');
+			}
+			else if (c < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(f + '0');
 
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(e + '0');
+				_putchar(f + '0');
+			}
 		}
-	      else
-		{
-		  _putchar(' ');
-		  _putchar(e + '0');
-		  _putchar(f + '0');
-		}
-
-	    }
-
-	  _putchar('\n');
+		_putchar('\n');
 	}
-    }
+	}
 }
