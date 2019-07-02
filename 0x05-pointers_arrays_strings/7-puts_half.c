a72b74753d76b7686a455f55c939a4737b18884a
Diego Vivas <dieguitovivas@hotmail.com>
2019-07-02 19:58:01 +0000

    sevenfix

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,36 +1,35 @@
 #include "holberton.h"
 /**
- * rev_string - check the code for Holberton School students.
+ * puts_half - check the code for Holberton School students.
  *@str:something
  * Return: Always 0.
  */
 void puts_half(char *str)
 {
-	
-        
-        int count = 0;
 
+	int count = 0;
 	int a;
-        while (str[count] != '\0')
-        {
-                count++;
-		
-        }
-	if((count % 2)==0)
-	{ 
-		for (a=(count/2); a < count ; a++)
+
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+
+	if ((count % 2) == 0)
+	{
+		for (a = (count / 2) ; a < count ; a++)
 		{
 			_putchar(str[a]);
-			_putchar(',');
 		}
 	}
-	else 
+
+	else
 	{
-		a= count -1;
-		for(a=((a/2)+1); a < count ; a++)
+		a = (count + 1);
+
+		for (a = (a / 2) ; a < count ; a++)
 		{
 			_putchar(str[a]);
-			_putchar(',');
 		}
 	}
 			_putchar('\n');
