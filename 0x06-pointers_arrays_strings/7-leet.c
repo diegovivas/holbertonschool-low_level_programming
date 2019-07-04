a9868f59e35c967071ba3d40b762661dd25141d8
Diego Vivas <dieguitovivas@hotmail.com>
2019-07-04 22:02:14 +0000

    sevenend

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,14 +7,14 @@
 char *leet(char *s)
 {
 	char arr1[6] = {"Aeotl"};
-	char arr2[6] = {"aEDTL"};
+	char arr2[6] = {"aEOTL"};
 	char arr3[6] = {"43071"};
 	int count = 0;
 	int count2;
 
 	while (s[count] != '\0')
 	{
-		for (count2 = 0; count2 < 6 ; count2++)
+		for (count2 = 0; count2 < 6; count2++)
 		{
 			if (s[count] == arr1[count2] || s[count] == arr2[count2])
 			{
