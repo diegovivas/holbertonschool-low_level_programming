901499cee1491ce55bc027c4ac8a8984d182edab
Diego Vivas <dieguitovivas@hotmail.com>
2019-07-04 22:44:46 +0000

    print number

---
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * print_number - check the code for Holberton School students.
+ *@n:character
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+	unsigned int a, b, c, d, f;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+	b = n;
+	c = 1;
+	for (a = 1 ; b > 9 ; a++)
+	{
+		b = b / 10;
+		c = c * 10;
+	}
+	for (f = 1; f <= a; f++)
+	{
+		d = n / c;
+		n = n % c;
+		c = c / 10;
+		_putchar(d + '0');
+
+	}
+}
