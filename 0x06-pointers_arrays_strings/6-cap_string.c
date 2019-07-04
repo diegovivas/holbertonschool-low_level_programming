960e61caa7b6fdb450a287250cdc402f4e0ccab7
Diego Vivas <dieguitovivas@hotmail.com>
2019-07-04 20:25:50 +0000

    sixdone

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * string_toupper - check the code for Holberton School students.
+ *@s:integer
+ * Return: Always 0.
+ */
+char *cap_string(char *s)
+{
+
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+		if (s[count] >= 'a' && s[count] <= 'z')
+		{
+			if(s[count-1] == ' ')
+			{
+                        s[count] = s[count] - 32;
+			}
+
+			if(s[count-1] == 9)
+                        {
+				s[count] = s[count] - 32;
+			}
+			if(s[count-1] == '\n')
+                        {
+				s[count] = s[count] - 32;
+			}
+			if(s[count-1] == '.')
+                        {
+				s[count] = s[count] - 32;
+			}
+		}
+		count++;
+	}
+        return (s);
+}
