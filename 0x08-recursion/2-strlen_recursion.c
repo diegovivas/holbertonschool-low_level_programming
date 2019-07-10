3c456e77f4f5f1e8e5b8479508b2b097b5ccb28e
Diego Vivas <dieguitovivas@hotmail.com>
2019-07-10 19:18:19 +0000

    add2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int _strlen_recursion(char *s)
+{
+	if(*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
