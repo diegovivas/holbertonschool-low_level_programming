3dfd0aefad3542f776fbfd4c65e2df41d39b889c
Diego Vivas <dieguitovivas@hotmail.com>
2019-07-10 19:30:34 +0000

    two and 3

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
- *
+ * _strlen_recursion - check the code for Holberton School students.
+ *@s:var
  * Return: Always 0.
  */
 int _strlen_recursion(char *s)
 {
-	if(*s == '\0')
+	if (*s == '\0')
 	{
 		return (0);
 	}
