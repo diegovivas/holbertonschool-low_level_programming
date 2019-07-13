b5f7ba250a66de66d7c17b109983af306cdd842c
diegovivas <dieguitovivas@hotmail.com>
2019-07-13 23:23:53 +0000

    3fixed

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,38 +1,9 @@
 #include <stdio.h>
-/**
- * _atoi - check the code for Holberton School students.
- *@s:do some
- * Return: Always 0.
- */
-int _atoi(char *s)
-{
-	int res = 0;
-	int sign = 1;
-	int i = 0;
-	int e = 1;
-	int f;
-
-	for (; s[i] != '\0'; ++i)
-	{
-		while (s[i] == '-' || s[i] == '=')
-		{
-			if (s[0] == '-')
-			{
-				f = -1;
-			}
-			sign = e * f;
-			e = sign;
-			i++;
-		}
-		res = res * 10 + (s[i] - '0');
-
-	}
-	return (sign * res);
-}
+#include <stdlib.h>
 /**
  * main - check the code for Holberton School students.
- *@argc:do some
- *@argv:dosome
+ *@argc:arguments passed
+ *@argv:array 
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
@@ -40,21 +11,18 @@ int main(int argc, char *argv[])
 	int i;
 	int mul = 1;
 
-	if (argc > 1)
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
 	{
 		for (i = 1 ; i < argc ; i++)
 		{
-			if (i > 2)
-			{
-				printf("Error\n");
-				return (1);
-			}
-			else
-			{
-				mul *= _atoi(argv[i]);
-			}
+			mul *= atoi(argv[i]);
 		}
-		printf("%d\n", mul);
 	}
+	printf("%d\n", mul);
 	return (0);
 }
