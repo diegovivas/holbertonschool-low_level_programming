465faddc06cf752da7b5b3adaef1c3e7e0d45c47
diegovivas <dieguitovivas@hotmail.com>
2019-07-13 23:28:50 +0000

    4fixed

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,38 +1,9 @@
 #include <stdio.h>
-/**
- * _atoi - check the code for Holberton School students.
- *@s:do some
- * Return: Always 0.
- */
-int _atoi(char *s)
-{
-	int res = 0;
-	int sign = 1;
-	int i = 0;
-	int e = 1;
-	int f;
-
-	for (; s[i] != '\0'; ++i)
-	{
-		while (s[i] == '-' || s[i] == '=')
-		{
-			if (s[0] == '-')
-			{
-				f = -1;
-			}
-			sign = e * f;
-			e = sign;
-			i++;
-		}
-		res = res * 10 + (s[i] - '0');
-
-	}
-	return (sign * res);
-}
+#include <stdliv.h>
 /**
  * main - check the code for Holberton School students.
- *@argc:do some
- *@argv:dosome
+ *@argc:argument
+ *@argv:array
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
@@ -52,7 +23,7 @@ int main(int argc, char *argv[])
 					a = 1;
 				}
 			}
-			mul += _atoi(argv[i]);
+			mul += atoi(argv[i]);
 		}
 		if (a == 1)
 		{
