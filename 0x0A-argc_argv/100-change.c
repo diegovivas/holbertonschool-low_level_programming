b25bbf3f5be7f62d4667acc1b391d2bbe9f96ef4
diegovivas <dieguitovivas@hotmail.com>
2019-07-15 03:38:29 +0000

    100change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,39 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * _isupper - check the code for Holberton School students.
- *@c: is c do something
+ * main - check the code for Holberton School students.
+ *@argc: arguments passed
+ *@argv: array into arguments
  * Return: Always 0.
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	float division;
-	int entero,resultado,numero,ciclo;
-	char monedas[5]={25,10,5,2,1};
+	double division;
+	unsigned int entero, resultado, numero, ciclo;
+	unsigned char monedas[5] = {25, 10, 5, 2, 1};
 
 	numero = atoi(argv[1]);
 	resultado = 0;
 	if (argc == 2)
 	{
 		ciclo = 0;
-		while(monedas[ciclo] != '\0')
+		while (monedas[ciclo] != '\0')
 		{
 			if (numero >= monedas[ciclo])
 			{
-				division = numero/monedas[ciclo];
+				division = numero / monedas[ciclo];
 				entero = division;
 				numero = numero - (monedas[ciclo] * entero);
 				resultado = resultado + entero;
 			}
 			ciclo++;
 		}
-		printf("%d\n", resultado);
-		return(0);
+		printf("%u\n", resultado);
+		return (0);
 	}
 	else
 	{
 		printf("Error\n");
-			return(1);
+			return (1);
 	}
 
 }
