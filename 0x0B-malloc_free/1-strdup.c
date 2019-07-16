b14fe4ccb927a1ad06813eb8e5bc8d99c5314133
Diego Vivas <dieguitovivas@hotmail.com>
2019-07-16 21:03:15 +0000

    strdupone

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ *@str: string source
+ * Return: Always 0.
+ */
+char *_strdup(char *str)
+{
+	char *str2;
+	char *punterosave;
+	int contador = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		while (str[contador])
+		{
+			contador++;
+		}
+		str2 = malloc(contador + 1);
+		punterosave = str2;
+		while (*str)
+		{
+			*punterosave++ = *src++;
+		}
+		return (str2);
+	}
+}
