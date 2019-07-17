9b07344f081c208e76223cd8c1214497bc665402
Diego Vivas <dieguitovivas@hotmail.com>
2019-07-17 02:06:46 +0000

    files

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - prints a grid of integers
+ * @height: the address of the two dimensional grid
+ * @width: width of the grid
+ * Return: Nothing.
+ */
+int **alloc_grid(int width, int height)
+{
+
+}
---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_grid - prints a grid of integers
+ * @grid: the address of the two dimensional grid
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
