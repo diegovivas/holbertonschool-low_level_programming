08650d659ee4a7d210be4ae964b0def6636daca5
Diego Vivas <dieguitovivas@hotmail.com>
2019-07-18 21:06:51 +0000

    arrayrange

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -18,12 +18,13 @@ int *array_range(int min, int max)
 	{
 		return (NULL);
 	}
+
 	pointer = malloc(counter * sizeof(int));
 	if (pointer == NULL)
 	{
 		return (NULL);
 	}
-	for (cont2 = 0; cont2 <= counter; cont2++)
+	for (cont2 = 0; min <= max; cont2++)
 	{
 		pointer[cont2] = min;
 		min++;
