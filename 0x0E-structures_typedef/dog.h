6ae9943cbcf262b478f704db6e2e449d59e75ac7
Diego Vivas <dieguitovivas@hotmail.com>
2019-07-22 22:14:51 +0000

    3fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,18 @@ struct dog
 	float  age;
 	char *owner;
 };
+/**
+ * struct d - Typedef for unsigned int
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+typedef struct d
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
