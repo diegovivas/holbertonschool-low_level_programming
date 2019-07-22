e1a49fec24901f5f4e59e7ff8903eec0ab0a3145
Diego Vivas <dieguitovivas@hotmail.com>
2019-07-22 21:01:35 +0000

    2fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,14 +16,16 @@ void print_dog(struct dog *d)
 		}
 		if (!((*d).owner))
 		{
-			printf("Owner: %s\n", (*d).owner);
+			printf("Owner: (nil)\n");
 		}
-
+		else
+		{
 		printf("Name: %s\n", (*d).name);
 		printf("Age: %f\n", (*d).age);
 		printf("Owner: %s\n", (*d).owner);
+		}
 	}
-	else 
+	else
 	{
 		return;
 	}
