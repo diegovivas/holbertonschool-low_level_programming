934698bcd65395d311db539f88c670fbcdafd762
Diego Vivas <dieguitovivas@hotmail.com>
2019-07-22 17:30:15 +0000

    preprocesor

---
+++ 0x0D-preprocesor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define NAME (__FILE__)
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf(NAME);
+	printf("\n");
+	return (0);
+}
---
+++ 0x0D-preprocesor/README.md
@@ -0,0 +1,4 @@
+README FILE ABOUT PREPROCESOR IN C
+What are macros and how to use them
+What are the most common predefined macros
+How to include guard your header files
