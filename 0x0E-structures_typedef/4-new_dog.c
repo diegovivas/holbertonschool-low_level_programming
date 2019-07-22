b470d4bc7d6016891cbbee98e57df075e3aa1d86
Diego Vivas <dieguitovivas@hotmail.com>
2019-07-22 22:50:10 +0000

    dog4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,7 +13,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_do;
 
-/*	new_do = malloc(sizeof(dog_t)); */
+	new_do = malloc(sizeof(dog_t)); 
 	if (!new_do)
 		return (NULL);
 	(*new_do).name = name;
