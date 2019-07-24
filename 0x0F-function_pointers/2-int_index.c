f7431deb5c075d64758b1a00ab1f4c8479eade5d
Diego Vivas <dieguitovivas@hotmail.com>
2019-07-24 18:24:34 +0000

    2done

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - return the iindex of the fisrt element
+ *@array: the integer to check
+ *@size: size
+ *@cmp: funtion pointer
+ * Return: 0 if false, something else otherwise.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int sizet = 0;
+	int eye;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	while (sizet < size)
+	{
+	eye = cmp(array[sizet]);
+	if (eye == 1)
+	{
+		break;
+	}
+	sizet++;
+	}
+	if (eye)
+	return (sizet);
+	else
+		return (-1);
+}
