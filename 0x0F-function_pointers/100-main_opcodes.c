6872bec14b81ffed8e848c6f47ecbdb1be5c38c0
Diego Vivas <dieguitovivas@hotmail.com>
2019-07-25 00:40:37 +0000

    chambonada

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * print_elem - prints an integer
- * @elem: the integer to print
- *
+ * main - chambonada
+ * @argc: the integer to print
+ *@argv: arguments
  * Return: Nothing.
  */
 int main(int argc, char *argv[])
 {
+	char a;
+
+	a = atoi(argv[a]);
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	if(*argv < 0)
+	if (argv[a] < 0)
 	{
 		printf("Error\n");
 		exit(0);
