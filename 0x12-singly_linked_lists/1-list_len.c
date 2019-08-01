588aaff2f0da09a0cd7f5f2e70699632a93a3b1c
diegovivas <dieguitovivas@hotmail.com>
2019-08-01 21:16:11 +0000

    Fix #1: betty style

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,18 +2,19 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- *
- *
- *
+ *list_len - return the number of elements in a linked list
+ *@h; data in a linked list 
+ *Return: number of elements
  */
 size_t list_len(const list_t *h)
 {
 	size_t elements;
+
 	elements = 0;
 	while (h)
 	{
 		elements++;
 		h = h->next;
-	}	
+	}
 	return (elements);
 }
