73d7e06894533d20cb5454cd03ab70a3a6d7483f
Diego Vivas <dieguitovivas@hotmail.com>
2019-08-06 21:19:02 +0000

    Feature #100: reverse listing

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * reverse_listint - check the code for Holberton School students.
+ * @head:head
+ * Return: head.
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *pointer1, *pointer2;
+
+	pointer1 = *head;
+	pointer2 = pointer1->next;
+	*head = pointer2->next;
+	pointer1->next = NULL;
+	while (pointer2->next)
+	{
+		pointer2->next = pointer1;
+		pointer1 = pointer2;
+		pointer2 = *head;
+		if ((*head)->next)
+		{
+			*head = (*head)->next;
+		}
+	}
+	(*head)->next = pointer1;
+	return (*head);
+}
