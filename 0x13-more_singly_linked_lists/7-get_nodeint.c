ea27fec99c03bc76d4c93a89b9ce2c0ee57ec1e1
Diego Vivas <dieguitovivas@hotmail.com>
2019-08-06 15:08:20 +0000

    FIX #7: return

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -21,8 +21,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 			pointer = pointer->next;
 			count++;
 		}
-		return (pointer);
 	}
 	else
 		return (NULL);
+	return (pointer);
 }
