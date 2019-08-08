afc3dc6554202096e2b50f37adc9434c5b834329
Diego Vivas <dieguitovivas@hotmail.com>
2019-08-08 23:17:51 +0000

    Feature #3: set bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * set_bit - set value of a bit at given index.
+ * @n: int
+ * @index: index to set
+ * Return: Always 0.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int *pointer;
+	unsigned long int ind = 1;
+
+	if (index > 64)
+	{
+		return (-1);
+	}
+	pointer = n;
+	ind <<= index;
+	*pointer += ind;
+	return (1);
+
+}
