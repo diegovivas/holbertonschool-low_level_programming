940f9ac3ffec699951adc407b9a91d1621508cfa
diegovivas <dieguitovivas@hotmail.com>
2019-08-13 22:58:23 +0000

    Feature #0: read and print

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+/**
+ * read_textfile - read a text file and prints it.
+ * @filename:name of file.
+ * @letters: characters that musth be read.
+ * Return: Always 0.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file;
+	int count2 = 0;
+	char buf[1024];
+
+	file = open(filename,O_RDONLY);
+	if (file == -1)
+	{
+		printf("FAIL");
+		exit(1);
+	}
+	count2 = read(file, buf, letters);
+	buf[count2] = '\0';
+	close(file);
+	printf("%s\n", buf);
+	return (count2);
+}
