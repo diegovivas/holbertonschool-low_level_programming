7f57e8faadf063906810253aaf8c1f15efbe592c
diegovivas <dieguitovivas@hotmail.com>
2019-08-14 01:48:18 +0000

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,9 +19,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	if (!text_content)
-		text_content = "";
 	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 00600);
+	if (!text_content)
+	{
+		return (1);
+	}
 	if (file == -1)
 		return (-1);
 
