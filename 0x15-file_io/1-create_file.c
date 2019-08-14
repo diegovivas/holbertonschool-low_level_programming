0e0e453132218438c5e3b71c470dee9c194fa951
diegovivas <dieguitovivas@hotmail.com>
2019-08-14 00:33:23 +0000

    Feature #1:createfile

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * create_file - create file with write and read permisions.
+ * @filename: filename
+ * @text_content: text  content
+ * Return: if succes 1 else -1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file;
+	int count2 = 0;
+
+	if (!filename)
+		return (-1);
+
+	file = open(filename, O_CREAT | O_RDWR, 00600);
+	if (file == -1)
+		return (-1);
+	while (text_content[count2])
+	{
+		count2++;
+	}
+
+	write(file, text_content, count2);
+	close(file);
+
+	return (1);
+}
