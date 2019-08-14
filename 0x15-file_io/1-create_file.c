f89e29f75265dc5a8a3983136c497cd0cffe3168
diegovivas <dieguitovivas@hotmail.com>
2019-08-14 00:50:12 +0000

    createfile

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,26 +14,28 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file,see;
+	int file, see;
 	int count2 = 0;
 
-	if (!filename)
+	if (!filename || !text_content)
 		return (-1);
 	see = open(filename, O_RDONLY);
-	if (see != -1)
+	if (see == -1)
 	{
-		return (-1);
-        }
-	file = open(filename, O_CREAT | O_RDWR, 00600);
-	if (file == -1)
-		return (-1);
-	while (text_content[count2])
-	{
-		count2++;
+		file = open(filename, O_CREAT | O_RDWR, 00600);
+		if (file == -1)
+			return (-1);
+		while (text_content[count2])
+		{
+			count2++;
+		}
+
+		write(file, text_content, count2);
+		close(file);
+		return (1);
 	}
-
-	write(file, text_content, count2);
-	close(file);
-
-	return (1);
+	else
+	{
+		return (-1);
+	}
 }
