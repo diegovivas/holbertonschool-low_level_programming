3328528b22f6381f6cca12164d9bf800b1d95fa3
diegovivas <dieguitovivas@hotmail.com>
2019-08-14 01:04:35 +0000

    createfile

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,7 +32,10 @@ int create_file(const char *filename, char *text_content)
 		count2++;
 	}
 
-	write(file, text_content, count2);
+	if (write(file, text_content, count2) != count2)
+	{
+		return (-1);
+	}
 	close(file);
 	return (1);
 }
