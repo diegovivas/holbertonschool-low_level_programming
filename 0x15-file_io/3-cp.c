f76ced26908ec24f63e7979b604941b24586b856
diegovivas <dieguitovivas@hotmail.com>
2019-08-14 05:02:46 +0000

    Feature #3:copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -27,7 +27,6 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	len = read(file1, buf, 1024);
 	close(file1);
 	file2 = open(av[2], O_WRONLY | O_CREAT | O_APPEND | O_TRUNC, 0600);
 	if (file2 == -1)
@@ -35,8 +34,8 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
-
-	wr = write(file2, buf, len), c1 = close(file1), c2 = close(file2);
+	while ((len = read(file1, buf, 1024)) != 0)
+		wr = write(file2, buf, len), c1 = close(file1), c2 = close(file2);
 	if (wr == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
