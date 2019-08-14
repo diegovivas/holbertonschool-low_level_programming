1a126253b41b1df3d789725ca2071fcd102aee93
diegovivas <dieguitovivas@hotmail.com>
2019-08-14 05:25:04 +0000

    Feature #3:copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -35,21 +35,20 @@ int main(int ac, char **av)
 		exit(99);
 	}
 	while ((len = read(file1, buf, 1024)) != 0)
-	{	wr = write(file2, buf, len);
-	if (wr == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		if (len == -1)
+		{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+			exit(98); }
+		wr = write(file2, buf, len);
+	if (wr == -1)
+	{dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99); }}
 	c1 = close(file1), c2 = close(file2);
 	if (c1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file1);
-		exit(100);
-	}
+	{		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file1);
+		exit(100); }
 	if (c2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file2);
-		exit(100);
-	}
+	{dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file2);
+		exit(100); }
 	return (0);
 }
