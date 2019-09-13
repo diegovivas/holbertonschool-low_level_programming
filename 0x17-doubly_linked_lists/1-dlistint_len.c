550565b7b867b70abac4c2882526ad55cf53b341
Diego Vivas <dieguitovivas@hotmail.com>
2019-09-13 21:21:51 +0000

    Feature #1: adding 1 file

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * dlistint_len - check the code for Holberton School students.
+ *@h: head
+ * Return: Always EXIT_SUCCESS.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *ojo;
+	size_t cont;
+
+	ojo = h;
+	while (ojo->next)
+	{
+		ojo = ojo->next;
+		cont++;
+	}
+	cont++;
+	return (cont);
+}
