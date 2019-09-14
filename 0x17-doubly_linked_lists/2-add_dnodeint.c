4fea49ecea22119777c83da043674ff4709d8ec6
diegovivas <dieguitovivas@hotmail.com>
2019-09-14 21:20:37 +0000

    Fix adding prevs

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -21,7 +21,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	ojo->next = NULL;
 	ojo->prev = NULL;
 	if (*head)
+	{
 		ojo->next = *head;
+		(*head)->prev = ojo;
+	}
 	*head = ojo;
 	return (*head);
 }
