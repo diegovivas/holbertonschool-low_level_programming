9e321d459d248a6ace58a2e6092d8215b8eedcf7
diegovivas <dieguitovivas@hotmail.com>
2019-09-14 21:57:29 +0000

    Feature #4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_dlistint - check the code for Holberton School students.
+ *@head: head
+ * Return: Nothing.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *puntero;
+
+	if(!head)
+		return;
+
+	puntero = head;
+	while (puntero)
+	{
+		puntero = puntero->next;
+		free(head);
+	head = puntero;
+	}
+	free(head);
+}
