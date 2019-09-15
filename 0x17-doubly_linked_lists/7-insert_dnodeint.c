6197b092f57dbd045a53a0dfeab7ebe8095ce86d
diegovivas <dieguitovivas@hotmail.com>
2019-09-15 00:00:02 +0000

    Fix first again

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -28,7 +28,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		if (idx == 0)
 		{			ojo->next = *h;
-			(*h)->prev = ojo;
+			if (*h)
+				(*h)->prev = ojo;
 			*h = ojo;
 		}
 		else
