0a54e0d7ecc82a26307ff17a60791039106acc88
diegovivas <dieguitovivas@hotmail.com>
2019-09-17 00:36:32 +0000

    Fix name .so

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P /tmp -q https://raw.githubusercontent.com/diegovivas/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/fuckso.so
-export LD_PRELOAD=/tmp/fucks.so
+export LD_PRELOAD=/tmp/fuckso.so
