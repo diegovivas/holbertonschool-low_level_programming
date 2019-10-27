cf822be96bc3187346dc674c5dae69ad7de1875b
diegovivas <dieguitovivas@hotmail.com>
2019-10-27 21:38:14 +0000

    o()

--- 0x1B-sorting_algorithms/1-insertion_sort_list.c
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -12,7 +12,7 @@ void insertion_sort_list(listint_t **list)
 	int flag = 0;
 
 	ojo = *list;
-	if (*list == NULL || (*list)->next == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 	while (ojo)
 	{
