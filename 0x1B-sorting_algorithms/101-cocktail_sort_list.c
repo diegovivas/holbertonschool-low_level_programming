fefe0b8cce4dc88c934c183e025c045488b0b587
diegovivas <dieguitovivas@hotmail.com>
2019-10-28 23:07:14 +0000

    betty style

--- 0x1B-sorting_algorithms/101-cocktail_sort_list.c
+++ 0x1B-sorting_algorithms/101-cocktail_sort_list.c
@@ -3,7 +3,11 @@
 #include "sort.h"
 listint_t *adelante(listint_t **list, listint_t **list2);
 listint_t *atras(listint_t **list, listint_t **list2);
-
+/**
+ * cocktail_sort_list - cocktailsort function
+ *@list: list to order
+ * Return: nothing
+ */
 void cocktail_sort_list(listint_t **list)
 {
 	listint_t *ojo;
@@ -24,12 +28,17 @@ void cocktail_sort_list(listint_t **list)
 		ojo = atras(list, &ojo);
 	}
 }
-
+/**
+ * adelante - sort elements from begin
+ *@list: list to print
+ *@list2: list to order
+ * Return: pointer to start next iteration
+ */
 listint_t *adelante(listint_t **list, listint_t **list2)
 {
 	listint_t *ret = NULL, *ojo, *ojo2, *ojo3 = *list2;
 
-	while(ojo3)
+	while (ojo3)
 	{
 		ojo = ojo3;
 		while (ojo->next != NULL && ojo->n > ojo->next->n)
@@ -55,33 +64,37 @@ listint_t *adelante(listint_t **list, listint_t **list2)
 	else
 		return (*list);
 }
-
+/**
+ * atras - Entry point
+ *@list: list to print
+ *@list2: list to sort
+ * Return: pointer to start next iteration
+ */
 listint_t *atras(listint_t **list, listint_t **list2)
 {
-        listint_t *ret = NULL, *ojo, *ojo2, *ojo3 = *list2;
+	listint_t *ret = NULL, *ojo, *ojo2, *ojo3 = *list2;
 
-        while(ojo3)
+	while (ojo3)
 	{
 		ojo = ojo3;
-                while (ojo->prev != NULL && ojo->n < ojo->prev->n)
+		while (ojo->prev != NULL && ojo->n < ojo->prev->n)
 		{
 			ret = ojo;
-                        ojo2 = ojo->prev;
-                        if (ojo2->prev != NULL)
+			ojo2 = ojo->prev;
+			if (ojo2->prev != NULL)
 				ojo2->prev->next = ojo;
-                        ojo->prev = ojo2->prev;
+			ojo->prev = ojo2->prev;
 			if (ojo->next != NULL)
 				ojo->next->prev = ojo2;
 			ojo2->next = ojo->next;
 			ojo2->prev = ojo;
-                        ojo->next = ojo2;
+			ojo->next = ojo2;
 			if ((*list)->prev != NULL)
 				*list = (*list)->prev;
 			print_list(*list);
-		        
 		}
-                ojo3 = ojo3->prev;
-        }
+		ojo3 = ojo3->prev;
+	}
 	if (ret != NULL)
 		return (ret);
 	else
