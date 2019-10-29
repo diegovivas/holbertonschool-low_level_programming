04cab46e9079746f0b9bacfd2e7539c4e29b2bc7
diegovivas <dieguitovivas@hotmail.com>
2019-10-29 23:43:08 +0000

    feature counting sort

---
+++ 0x1B-sorting_algorithms/102-counting_sort.c
@@ -0,0 +1,71 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "sort.h"
+
+int *ordenar(int *at, int temp, size_t size, int *array);
+
+void counting_sort(int *array, size_t size)
+{
+	size_t cont = 0;
+	int n1, n2, temp = 0;
+	int *at, *ar2;
+
+	for (;cont < size - 1; cont++)
+	{
+		if (temp < array[cont])
+			temp = array[cont];
+	}
+	at = malloc((temp + 1) * sizeof(int));
+	if (at == NULL)
+		return;
+	at = ordenar(at, temp, size, array);
+	ar2 = malloc((size + 1) * sizeof(int));
+	if (ar2 == NULL)
+		return;
+
+	cont = 0;
+	while (cont < size)
+	{
+		n1 = array[cont];
+		n2 = at[n1];
+		ar2[n2 - 1] = n1;
+		at[n1]--;
+		cont++;
+	}
+	cont = 0;
+	while (cont < size)
+	{
+		array[cont] = ar2[cont];
+		cont++;
+	}
+}
+int *ordenar(int *at, int temp, size_t size, int *array)
+{
+	int temp2, cont2 = 0;
+	size_t cont3;
+
+	while (cont2 <= temp)
+	{
+                at[cont2] = 0;
+                cont2++;
+	}
+	cont2 = 0;
+	cont3 = 0;
+	while (cont3 < size)
+	{
+		temp2 = array[cont3];
+		at[temp2]++;
+		cont3++;
+	}
+        while (cont2 <= temp)
+	{
+		if (at[cont2 - 1])
+                        at[cont2] = at[cont2] + at[cont2 - 1];
+		if (cont2 < temp)
+			printf("%d, ", at[cont2]);
+		else
+			printf("%d\n", at[cont2]);
+                cont2++;
+	}
+	return at;
+}
