7c59c806ae313a2129e099e32b0f4796a05e15ec
diegovivas <dieguitovivas@hotmail.com>
2019-11-15 23:43:48 +0000

    island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,15 +9,13 @@ def island_perimeter(grid):
         for idx2, horizontal in enumerate(vertical):
             maxvertical = len(vertical) - 1
             if horizontal == 1:
-                if idx1 != 0 and grid[idx1 - 1][idx2] == 0:
+                if idx1 != 0 or grid[idx1 - 1][idx2] == 0:
                     perimeter += 1
-                if idx1 != maxidx and grid[idx1 + 1][idx2] == 0:
+                if idx1 != maxidx or grid[idx1 + 1][idx2] == 0:
                     perimeter += 1
-                if idx2 != 0 and grid[idx1][idx2 - 1] == 0:
+                if idx2 != 0 or grid[idx1][idx2 - 1] == 0:
                     perimeter += 1
-                if idx2 == 0:
-                    perimeter += 1
-                if idx2 != maxvertical and grid[idx1][idx2 + 1] == 0:
+                if idx2 != maxvertical or grid[idx1][idx2 + 1] == 0:
                     perimeter += 1
             else:
                 pass
